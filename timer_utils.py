3206a8d0ef818cdef219c05cf89d7689fe08e1cd
kb638 <151879669+kb638@users.noreply.github.com>
2023-12-21 17:05:36 -0500

    Update timer_utils.py

--- timer_utils.py
+++ timer_utils.py
@@ -9,20 +9,17 @@ def display_countdown(nseconds):
     - nseconds: Total duration of the countdown in seconds.
     """
     start_time = time()
+    sleep_interval = 5
 
     while True:
-        sleep_interval = 5
         sleep(sleep_interval)
-
-        now = time()
-        elapsed_time = now - start_time
+        elapsed_time = time() - start_time
 
         if elapsed_time > nseconds:
             break
 
         time_remaining = nseconds - elapsed_time
         print(f"{time_remaining:.1f}s left...")
-
 
 # Example usage:
 # display_countdown(60)
