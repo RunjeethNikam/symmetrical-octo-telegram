1532d14e75c8183bb7f6562d23c0c960a17553f6
Gaurav Jangid <60379544+gaurav700@users.noreply.github.com>
2023-12-20 21:33:14 -0500

    Update link_vm.sh

--- setup/link_vm.sh
+++ setup/link_vm.sh
@@ -1,3 +1,6 @@
-source ./../configurations.sh
-source common_utils.sh
-link_vms ${HOST_1} ${HOST_2} ${GCLOUD_PROJECT} ${ZONE}
+# Import configuration files
+source ./../custom_config.sh
+source utilities.sh    # COMMON_UTILITIES.SH 
+
+# Connect VMs with specified parameters
+establish_connection "$CUSTOM_HOST_1" "$CUSTOM_HOST_2" "$CUSTOM_PROJECT" "$CUSTOM_ZONE"
