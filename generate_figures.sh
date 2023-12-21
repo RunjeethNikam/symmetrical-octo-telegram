c1ccea1502621f673bf6d7e45590cd4f4c70a9da
Runjeeth Nikam <65700768+RunjeethNikam@users.noreply.github.com>
2023-12-21 18:11:29 -0500

    Update generate_figures.sh

--- generate_figures.sh
+++ generate_figures.sh
@@ -3,13 +3,13 @@
 # This script generates and organizes figures related to the experiment.
 
 # Create a directory to store figures
-mkdir -p ./figures
+mkdir -p ./experiment_results
 
 # Run the figure5.sh script with the 'iperf' argument
 sudo ./figure5.sh
 
 # Copy the generated image to the main figures directory
-cp ./figure5_iperf/figure5_iperf.png ./figures/figure5_iperf_vms.png
+cp ./figure5_iperf/figure5_iperf.png ./experiment_results/figure5_iperf_vms.png
 
 # Display a completion message
 echo "Figures generated and organized successfully in the ./figures directory."
