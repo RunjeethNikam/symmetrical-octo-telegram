2f69d47a8285ef51b667d138a5782e8168abda22
kb638 <151879669+kb638@users.noreply.github.com>
2023-12-21 17:03:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,49 +22,51 @@ Parameters:
 Example:
     python script.py --bw-net 10 --delay 35 --dir /path/to/output --time 20
 """
-from argparse import ArgumentParser
 
+from argparse import ArgumentParser
 from experiment import run_experiment
 
-parser = ArgumentParser(description="BBR Experiment Script")
+def parse_args():
+    parser = ArgumentParser(description="BBR Experiment Script")
 
-parser.add_argument(
-    "--bw-net",
-    "-b",
-    type=float,
-    help="Bandwidth of the bottleneck (network) link in megabits per second (Mb/s)",
-    required=True,
-)
-parser.add_argument(
-    "--delay",
-    type=float,
-    help="Link propagation delay in milliseconds (ms)",
-    required=True,
-)
-parser.add_argument(
-    "--dir", "-d", help="Directory to store experiment outputs", required=True
-)
-parser.add_argument(
-    "--time",
-    "-t",
-    help="Duration in seconds to run the experiment (default: 10 seconds)",
-    type=int,
-    default=10,
-)
-parser.add_argument(
-    "--maxq",
-    type=int,
-    help="Maximum buffer size of the network interface in packets (optional)",
-)
-parser.add_argument("--flow-type", help="Type of flow for the experiment (optional)")
-parser.add_argument(
-    "--environment", help="Environment setting for the experiment (optional)"
-)
-parser.add_argument(
-    "--dest-ip", help="Destination IP address for the experiment (optional)"
-)
+    parser.add_argument(
+        "--bw-net",
+        "-b",
+        type=float,
+        help="Bandwidth of the bottleneck (network) link in megabits per second (Mb/s)",
+        required=True,
+    )
+    parser.add_argument(
+        "--delay",
+        type=float,
+        help="Link propagation delay in milliseconds (ms)",
+        required=True,
+    )
+    parser.add_argument(
+        "--dir", "-d", help="Directory to store experiment outputs", required=True
+    )
+    parser.add_argument(
+        "--time",
+        "-t",
+        help="Duration in seconds to run the experiment (default: 10 seconds)",
+        type=int,
+        default=10,
+    )
+    parser.add_argument(
+        "--maxq",
+        type=int,
+        help="Maximum buffer size of the network interface in packets (optional)",
+    )
+    parser.add_argument("--flow-type", help="Type of flow for the experiment (optional)")
+    parser.add_argument(
+        "--environment", help="Environment setting for the experiment (optional)"
+    )
+    parser.add_argument(
+        "--dest-ip", help="Destination IP address for the experiment (optional)"
+    )
 
+    return parser.parse_args()
 
 if __name__ == "__main__":
-    args = parser.parse_args()
+    args = parse_args()
     run_experiment(args)
